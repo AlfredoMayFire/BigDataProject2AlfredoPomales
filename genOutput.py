d59f082f0b5cafd9072da242e160bb20e35f7d45
A_Pomales3 <{alfredopomales@gmail.com}>
2018-12-10 23:05:45 -0400

    Fixed conflicting reserved words

--- genOutput.py
+++ genOutput.py
@@ -13,18 +13,22 @@ def genOutput():
     df = spark.sql("use default")
 
     #Exercise 4a
-    #df = spark.sql("select hashtag, sum(total) as count from hashtagTable where timestamp between cast('2018-12-06 12:00:00' as timestamp) and cast('2018-12-06 13:00:00' as timestamp) group by hashtag order by total desc limit 10")
+    #df = spark.sql("select hashtag, sum(total) as totalNum from hashtagTable where timestamp between cast('2018-12-06 12:00:00' as timestamp) and cast('2018-12-06 13:00:00' as timestamp) group by hashtag order by total desc limit 10")
     #Exercise 4b
-    #df = spark.sql("select word, sum(total) as count from keywordTable where timestamp between cast('2018-12-06 12:00:00' as timestamp) and cast('2018-12-06 13:00:00' as timestamp) group by word order by total desc limit 10")
+    #df = spark.sql("select word, sum(total) as totalNum from keywordTable where timestamp between cast('2018-12-06 12:00:00' as timestamp) and cast('2018-12-06 13:00:00' as timestamp) group by word order by total desc limit 10")
     #Exercise 4c
-    #df = spark.sql("select name, sum(total) as count from snTable where timestamp between cast('2018-12-06 12:00:00' as timestamp)- INTERVAL 12 HOUR and cast('2018-12-06 12:00:00' as timestamp) group by name order by total desc limit 10")
+    #df = spark.sql("select name, sum(total) as totalNum from snTable where timestamp between cast('2018-12-06 12:00:00' as timestamp)- INTERVAL 12 HOUR and cast('2018-12-06 12:00:00' as timestamp) group by name order by total desc limit 10")
     #Exercise 5 Day 1
-    #df = spark.sql("select keyword,sum(total) as count from ocurrencesTable where timestamp between cast('2018-12-06 13:00:00' as timestamp) and cast('2018-12-06 12:00:00' as timestamp) group by keyword order by total desc limit 10")
+    #df = spark.sql("select keyword,sum(total) as totalNum from ocurrencesTable where timestamp between cast('2018-12-06 13:00:00' as timestamp) and cast('2018-12-06 12:00:00' as timestamp) group by keyword order by total desc limit 10")
     #Exercise 5 Day 2
-    #df = spark.sql("select keyword,sum(total) as count from ocurrencesTable where timestamp between cast('2018-12-07 22:00:00' as timestamp) and cast('2018-12-07 21:00:00' as timestamp) group by keyword order by total desc limit 10")
+    #df = spark.sql("select keyword,sum(total) as totalNum from ocurrencesTable where timestamp between cast('2018-12-07 22:00:00' as timestamp) and cast('2018-12-07 21:00:00' as timestamp) group by keyword order by total desc limit 10")
     #Exercise 5 Day 3
 
-    df = spark.sql("select keyword,sum(total) as count from ocurrencesTable where timestamp between cast('2018-12-08 1:00:00' as timestamp) and cast('2018-12-08 00:00:00' as timestamp) group by keyword order by total desc limit 10")
+    df = spark.sql("select keyword,sum(total) as totalNum from ocurrencesTable where timestamp between cast('2018-12-08 1:00:00' as timestamp) and cast('2018-12-08 00:00:00' as timestamp) group by keyword order by total desc limit 10")
+
+
+
+
     df.show()
 
     #Pon archivo de salida, no puede existir
